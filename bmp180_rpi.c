6992f403a9d315959780d5be569d3064b88f30b0
wouterwashere <public@hitzonline.se>
2016-10-06 18:09:18 +0200

    Update bmp180_rpi.c

--- bmp180_rpi.c
+++ bmp180_rpi.c
@@ -2,13 +2,13 @@
 
 int main(int argc, char **argv)
 {
-	bmp180_get_cal_param();
-	temperature = bmp180_get_temperature(bmp180_get_ut());
-	pressure = bmp180_calpressure(bmp180_get_up());
-	height = atoi(argv[1]);
-	pressure_at_sea_level = bmp180_pressure_at_sea_level(pressure);
-	printf("Temperature\t\t%0.1f\tdegrees Celsius\n", ((float)temperature / 10));
-	printf("Pressure\t\t%0.1f\thPa\n", ((float)pressure / 100));
-	printf("Pressure at sea level\t%0.1f\thPa\n", ((float)pressure_at_sea_level / 100));
-	return 0;
+        bmp180_get_cal_param();
+        temperature = bmp180_get_temperature(bmp180_get_ut());
+        pressure = bmp180_calpressure(bmp180_get_up());
+        height = atoi(argv[1]);
+        pressure_at_sea_level = bmp180_pressure_at_sea_level(pressure);
+        printf("Temperature\t\t%0.1f\tdegrees Celsius\n", (float)temperature / 10);
+        printf("Pressure\t\t%0.1f\thPa\n", (float)pressure / 100);
+        printf("Pressure at sea level\t%0.1f\thPa\n", pressure_at_sea_level);
+        return 0;
 }
